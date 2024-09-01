ace8eb2ec913618d03625c39dd5ce745da9720d4
saikiran2111 <saikiran211101@gmail.com>
2024-09-01 12:05:53 +0530

    Basics and Datatypes

---
+++ basics.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cin>>str;
+
+    int a,b;
+    cin>>a>>b;
+
+    long c = 100000000;
+    long long d = 10000000000000000;
+
+    float e = 4.5;
+
+    string myName;
+    getline(cin,myName);
+
+    char f = 't';
+
+
+
+    cout<<"Hello world"<<"\n";
+    cout<<str<<"\n";
+    cout<<a<<" "<<b<<" "<<a+b<<"\n";
+    cout<<c<<"\n";
+    cout<<d<<"\n";
+    cout<<e<<"\n";
+    cout<<myName<<"\n";
+    cout<<f<<"\n";
+
+    return 0;
+}
